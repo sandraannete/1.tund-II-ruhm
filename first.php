7d8d437bec9327fe01bb83027cad7aef83f21177
sandtaga <sandtaga@greeny.cs.tlu.ee>
2016-09-06 15:17:29 +0300

    added my first file

---
+++ first.php
@@ -0,0 +1,19 @@
+<?php
+
+$firstName = "Sandra";
+$lastName = "Tagam";
+
+// echo trükib välja muutuja veebilehe
+
+//echo $firstName;
+//echo " ";
+//echo $lastName;
+
+// punkt liidab stringe
+
+echo $firstName." ".$lastName
+?>
+
+
+
+
